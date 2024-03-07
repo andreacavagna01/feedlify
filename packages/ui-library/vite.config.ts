208ec2a030b3335f2fa4616d77046c21ea785309
andreacavagna <andrea.cavagna@noovolari.com>
2024-03-07 10:03:57 +0100

    added crud for item in backend

---
+++ packages/ui-library/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({ include: ['lib'] })
+  ],
+  build: {
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+    },
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'), formats: ['es'],
+    }
+  }
+})
