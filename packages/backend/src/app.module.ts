a6522c0db4c11f61c6e04310d6f2afc4e729c19b
Andrea Cavagna <a.cava94@gmail.com>
2024-05-28 07:10:37 +0000

    added backend authentication with supertokens

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { ItemService } from './item/item.service';
 import { ItemModule } from './item/item.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Item } from './item/entities/item.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +20,19 @@ import { Item } from './item/entities/item.entity';
       database: 'feedlify',
       entities: [Item],
       synchronize: true,
+    }),
+    AuthModule.forRoot({
+      // https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+      connectionURI: 'https://try.supertokens.com',
+      // apiKey: <API_KEY(if configured)>,
+      appInfo: {
+        // Learn more about this on https://supertokens.com/docs/thirdparty/appinfo
+        appName: 'Feedlify',
+        apiDomain: 'http://localhost:3000',
+        websiteDomain: 'http://localhost:6006',
+        apiBasePath: '/auth',
+        websiteBasePath: '/auth',
+      },
     }),
   ],
   controllers: [AppController, ItemController],
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import supertokens from 'supertokens-node';
+import { SupertokensExceptionFilter } from './auth/auth.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['verbose'],
   });
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:6006'],
+    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Feedlify Swagger')
@@ -16,6 +22,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalFilters(new SupertokensExceptionFilter());
 
   await app.listen(3000);
 }
